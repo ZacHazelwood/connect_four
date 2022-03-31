fe9ca4e58508ff0885154dbfa56c19f9e3379354
Zachary Hazelwood <zhaze001@gmail.com>
2022-03-31 17:37:20 -0600

    Add game_spec

---
+++ spec/game_spec.rb
@@ -0,0 +1,20 @@
+require './lib/game'
+require './lib/board'
+require 'pry'
+
+describe Game do
+
+  it "exists" do
+    game = Game.new
+
+    expect(game).to be_an_instance_of(Game)
+  end
+
+  it "opens with welcome message" do
+    game = Game.new
+binding.pry
+    game.game_open
+    expect().to
+  end
+
+end
