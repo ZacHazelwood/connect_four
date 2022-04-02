d0acbe62d61238cea527e62edf07e7530cae6dfd
Zachary Hazelwood <zhaze001@gmail.com>
2022-04-02 12:11:41 -0600

    feat: Assign column positions

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,12 +3,25 @@ require './lib/board'
 
 class Turn
 
-attr_reader :board, :player_type
+attr_reader :board, :player_type, :column, :player_input
 
   def initialize(board, player_type)
     @player_type = player_type
     @board = board
+    @column = []
+    @player_input = ""
   end
 
-  
+  def assign_column(player_input)
+    columns = {
+        "A" => @board.full_board.transpose[0],
+        "B" => @board.full_board.transpose[1],
+        "C" => @board.full_board.transpose[2],
+        "D" => @board.full_board.transpose[3],
+        "E" => @board.full_board.transpose[4],
+        "F" => @board.full_board.transpose[5],
+        "G" => @board.full_board.transpose[6]
+      }
+      @column = columns[player_input]
+  end
 end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -12,5 +12,23 @@ describe Turn do
     expect(turn).to be_an_instance_of(Turn)
   end
 
+  it "can assign player input to a column position" do
+    board = Board.new
+    player_1 = Player.new(:human)
+    turn = Turn.new(board, player_1.type)
+
+    turn.assign_column("A")
+    expect(turn.column).to eq([".", ".", ".", ".", ".", "."])
+  end
+
+  it "can transpose columns from the full board" do
+    board = Board.new
+    player_1 = Player.new(:human)
+    turn = Turn.new(board, player_1.type)
+
+    turn.board.add_x(5, 0)
+    turn.assign_column("A")
+    expect(turn.column).to eq([".", ".", ".", ".", ".", "X"])
+  end
 
 end
