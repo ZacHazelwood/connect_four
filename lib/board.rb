1fd67a71f99a2b8dec412f5243563c5c17663025
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-04-02 15:42:04 -0700

    test:checked all horizon tests passed

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Board
 
 attr_reader :full_board, :board_column
@@ -21,9 +22,43 @@ attr_reader :full_board, :board_column
     end
   end
 
+  def has_won_horizontally?(full_board)
+     counter = 0
+     full_board.each_with_index do |row, row_index|
+       row.each_with_index do |cell, cell_index|
+         if counter == 4
+           return true
+         end
+         if cell_index == 3 && cell == "x" || cell_index == 3 && cell == "o"
+           row.each_with_index do |cell_position, index|
+             if cell_position == "x"
+               counter += 1
+               if counter == 4
+                 return true
+               end
+            #   binding.pry
+               if row[index + 1] != "x" && counter != 4
+                 counter = 0
+               end
+             end
+             if cell_position == "o"
+               counter += 1
+               if counter == 4
+                 return true
+               end
+                if row[index + 1] != "o" && counter != 4
+                  counter = 0
+                end
+             end
+           end
+         end
+      end
+    end
+    return false
+  end
 
 
-  def has_won_horizontally?
+  def has_won_vertically?
     board_column = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
     a = []
     b = []
@@ -32,31 +67,52 @@ attr_reader :full_board, :board_column
     e = []
     f = []
     g = []
-     full_board = [
-       [".",".",".",".",".",".","."],
-       [".",".",".",". ",".",".","."],
-       [".",".",".",".",".",".","."],
-       [".",".",".",".",".",".","."],
-       [".",".",".",".",".",".","."],
-       [".",".","x","x","x","x","."]
-     ]
+    full_board = [
+      [".",".",".",".",".",".","."],
+      [".",".",".",".","x",".","."],
+      [".",".",".","x","x",".","."],
+      [".",".",".","o","x",".","."],
+      [".",".",".","o","x",".","."],
+      [".",".",".","x","o",".","."]
+    ]
      counter = 0
 
-     full_board.each_with_index do |row, row_index|
-       row.each_with_index do |cell, cell_index|
-         if cell_index == 3 && cell == "x"
-           row.each do |x_index|
-             if x_index == "x"
-               counter += 1
-               if counter == 4
-                 return true
-               end
-             elsif x_index != "x"
-               counter = 0
-             end
-           end
-           # puts "#{row_index} x #{cell_index}"
-         end
+    full_board.each_with_index do |row, row_index|
+      row.each_with_index do |cell, cell_index|
+          if cell_index == 0
+            a << cell
+          end
+          if cell_index == 1
+            b << cell
+          end
+          if cell_index == 2
+            c << cell
+          end
+          if cell_index == 3
+            d << cell
+          end
+          if cell_index == 4
+            e << cell
+          end
+          if cell_index == 5
+            f << cell
+          end
+          if cell_index == 6
+            g << cell
+          end
+      end
+    end
+    turn_vertical_board = [a, b, c, d, e, f, g]
+    turn_vertical_board.each do |array|
+      array.each do |x_index|
+        if x_index == "x"
+          counter += 1
+          if counter == 4
+            return true
+          end
+        elsif x_index != "x"
+          counter = 0
+        end
       end
     end
     puts counter
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -22,17 +22,96 @@ end
 
   it "player connects 4 pieces horizontally" do
     board = Board.new
+    full_board = [
+      [".",".",".",".",".",".","."],
+      [".",".",".",".",".",".","."],
+      [".",".",".",".",".",".","."],
+      [".",".",".",".",".",".","."],
+      [".","x","o",".",".",".","."],
+      [".","o","x","x","x","x","o"]
+    ]
 
-    expect(board.has_won_horizontally?).to eq true
+    full_board1 = [
+      [".",".",".",".",".",".","."],
+      [".",".",".",".",".",".","."],
+      [".",".",".",".",".",".","."],
+      [".",".","x",".",".",".","."],
+      [".","x","o","o",".",".","."],
+      [".","o","x","o","x","x","o"]
+    ]
+
+    full_board2 = [
+      [".",".",".",".",".",".","."],
+      [".",".",".",".",".",".","."],
+      [".",".",".",".",".",".","."],
+      [".",".","x",".",".",".","."],
+      [".","x","o","o",".",".","."],
+      [".","o","x","o","o","o","o"]
+    ]
+
+    full_board3 = [
+      [".",".",".",".",".",".","."],
+      [".",".",".",".",".",".","."],
+      [".",".",".",".",".",".","."],
+      [".",".","x",".",".",".","."],
+      [".","x","o","o",".",".","."],
+      [".","o","x","x","x","o","o"]
+    ]
+
+    expect(board.has_won_horizontally?(full_board)).to eq true
+    expect(board.has_won_horizontally?(full_board1)).to eq false
+    expect(board.has_won_horizontally?(full_board2)).to eq true
+    expect(board.has_won_horizontally?(full_board3)).to eq false
   end
 
+  it "player connects 4 pieces vertically" do
+    board = Board.new
+    full_board = [
+      [".",".","x",".",".",".","."],
+      [".",".","x",".",".",".","."],
+      [".",".","x",".",".",".","."],
+      [".",".","x",".",".",".","."],
+      [".","x","o",".",".",".","."],
+      [".","o","x","x","x","x","o"]
+    ]
 
+    full_board1 = [
+      [".",".",".",".",".",".","."],
+      [".",".",".",".",".",".","."],
+      [".",".",".",".",".",".","."],
+      [".",".","x","o",".",".","."],
+      [".","x","o","o",".",".","."],
+      [".","o","x","o","x","x","o"]
+    ]
 
+    full_board2 = [
+      [".",".",".",".",".",".","."],
+      [".",".",".",".",".",".","."],
+      [".",".",".",".","o",".","."],
+      [".",".","x","x","o",".","."],
+      [".","x","o","o","o",".","."],
+      [".","o","x","o","o","o","o"]
+    ]
 
+    full_board3 = [
+      [".",".",".",".",".",".","."],
+      [".",".",".",".",".",".","."],
+      [".",".",".",".",".",".","."],
+      [".",".","x",".",".",".","."],
+      [".","x","o","o",".",".","."],
+      [".","o","x","x","x","o","o"]
+    ]
 
-# The player connects four of their pieces horizontally, vertically, or diagonally.
-# The computer connects four of their pieces horizontally, vertically, or diagonally.
-# The player cannot select a valid column.
-# The program reports the appropriate endgame status
-# Game returns user back to the Main Menu
+    expect(board.has_won_vertically?(full_board)).to eq true
+    expect(board.has_won_vertically?(full_board1)).to eq false
+    expect(board.has_won_vertically?(full_board2)).to eq true
+    expect(board.has_won_vertically?(full_board3)).to eq false
+  end
+
+  xit "player connects 4 pieces diagonally" do
+    board = Board.new
+
+    expect(board.has_won_diagonally?).to eq true
+  end
+
 end
