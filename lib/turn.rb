4b10b5fdd2cc22f791317d432097deb6fc35c049
Zachary Hazelwood <zhaze001@gmail.com>
2022-04-02 14:36:43 -0600

    feat:Can take input to assign column

--- lib/turn.rb
+++ lib/turn.rb
@@ -12,6 +12,7 @@ attr_reader :board, :player_type, :column, :player_input
     @player_input = ""
   end
 
+
   def assign_column(player_input)
     columns = {
         "A" => @board.full_board.transpose[0],
@@ -43,4 +44,38 @@ attr_reader :board, :player_type, :column, :player_input
   def column_lowest
     low_point = self.space_empty - 1
   end
+
+  def take_turn
+    if @player_type == :human
+      puts "Please select a column. Select A - G"
+      @player_input = gets.chomp.upcase
+    elsif @player_tpye == :computer
+      puts "The computer will select a column."
+      @player_input = (["A","B","C","D","E","F","G"]).sample
+    end
+  end
+
+  def valid_input?
+    ["A","B","C","D","E","F","G"].include?(@player_input)
+  end
 end
+
+
+# This just didn't work, had too many conditionals and was throwing errors in my terminal.
+# So I thought to condense the #take_turn method to just ask for input from the user,
+# and will check for valididty of the input when asking for the turn to commence.
+
+  # def take_turn
+  #   if @player_type == :human
+  #     puts "Please choose a column by typing it's letter value. Select A - G"
+  #     @player_input = gets.chomp.upcase
+  #     until player_input == "A" || player_input == "B" || player_input == "C" || player_input == "D" || player_input == "E" || player_input == "F" || player_input == "G"
+  #       puts "----- Invalid Input. Please try again. Select A - G -----"
+  #       @player_input = gets.chomp.upcase
+  #     end
+  #   elsif @player_type == :computer
+  #     puts "The computer will select a column."
+  #     @player_input = (["A", "B", "C", "D", "E", "F", "G"]).sample
+  #     # .sample selects a random element within an array
+  #   end
+  # end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,6 +1,7 @@
 require './lib/turn'
 require './lib/player'
 require './lib/board'
+require 'pry'
 
 describe Turn do
 
@@ -67,4 +68,38 @@ describe Turn do
     expect(turn.column_lowest).to eq(5)
   end
 
+  it "can accept player input" do
+    board = Board.new
+    player_1 = Player.new(:human)
+    turn = Turn.new(board, player_1.type)
+
+    turn.stub(:gets).and_return("A")
+    # Searched for ways to test user input and found .stub and comparable mocks
+    # This allows the test to assign a gets.chomp value directly within the test
+    expect(turn.take_turn).to eq("A")
+  end
+
+  it "can take input and assign a column" do
+    board = Board.new
+    player_1 = Player.new(:human)
+    turn = Turn.new(board, player_1.type)
+
+    turn.take_turn
+    allow(turn).to receive(:gets).and_return("A")
+    # turn.stub(:gets).and_return("A")
+    # Was receiving an error stating that the syntax has changed
+    # My research led me to discover the allow() method, which functions the same
+    expect(turn.assign_column("A")).to eq([".", ".", ".", ".", ".", "."])
+  end
+
+  it "randomly selects column for computer" do
+    board = Board.new
+    player_1 = Player.new(:computer)
+    turn = Turn.new(board, player_1.type)
+
+    turn.take_turn
+    turn.stub(:select).and_return("A")
+    # .stub(:select) allows for the test to select one of the variables within the player_input array available to the computer
+    expect(turn.assign_column("A")).to eq([".", ".", ".", ".", ".", "."])
+  end
 end
